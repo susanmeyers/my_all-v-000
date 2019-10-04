093265c1bd7eb0876d2835a770428560780e0de9
Susan Meyers <smmeyers24@gmail.com>
2019-10-04 03:20:12 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -6,7 +6,12 @@ def my_all?(collection)
   while i < collection.length
   block_return_values << yield(collection[i])  
     i = i + 1
+  end
   
+  if block_return_values.include?(false)
+    false
+  else
+    true
   end
 
 end
